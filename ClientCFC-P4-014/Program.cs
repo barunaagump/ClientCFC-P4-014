ef107dcc5e4979d982adc897d11e85782fec3c5d
baruna <baruna@NEBULA>
2020-10-25 23:34:04 +0700

    Add project files.

---
+++ ClientCFC-P4-014/Program.cs
@@ -0,0 +1,31 @@
+﻿using ServiceMtk_P1_014;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCFC_P4_014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+           ChannelFactory<IMatematika>(bind, "http://localhost:1907");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+        }
+    }
+}
